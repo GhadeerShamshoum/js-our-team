e404982b6da13cbb09296a9947a483439a0f06e9
Ghadeer <ghaora@gmail.com>
2022-01-10 18:11:22 +0200

    bonus

--- main.js
+++ main.js
@@ -3,55 +3,97 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
 
-itemsContainer = '';
+
+
 let teamCard = document.querySelector('.team-card');
-console.log(team.name)
-for(let i=0; i<team.length; i++){
-  itemsContainer +=` <div class="team-card">
+let teamContainer =document.querySelector('.team-container');
+let button = document.getElementById('addMemberButton');
+
+
+
+
+
+button.addEventListener("click", function(){
+  
+  let newName  = document.getElementById('name').value;
+  let newRole = document.getElementById('role').value;
+  let newImg = document.getElementById('image').value;
+  console.log(newRole, newName, newImg)
+  const newCard ={
+    name:newName,
+    role:newRole,
+    image:newImg,
+  }
+  teamCard +=` <div class="team-card">
     <div class="card-image">
-      <img
-        src='"${team[i].image}"'
-        alt='"${team[i].image}"'
-      />
+    <img
+    src="${newImg}"
+    alt="${newImg}"
+    
+    />
     </div>
     <div class="card-text">
-      <h3>${team[i].name}</h3>
-      <p>${team[i].role}</p>
+    <h3>${newName}</h3>
+    <p>${newRole}</p>
     </div>
-  </div>`;
-}
+    </div>`;
+    teamContainer.innerHTML= teamCard;
 
+  team.push(newCard); 
+  console.log(team)
+  
+});
 
-teamCard.innerHTML = itemsContainer;
+  
+  for(let i=0; i<team.length; i++){
+    teamCard +=` <div class="team-card">
+    <div class="card-image">
+    <img
+    src="${team[i].image}"
+    alt="${team[i].image}"
+    
+    />
+    </div>
+    <div class="card-text">
+    <h3>${team[i].name}</h3>
+    <p>${team[i].role}</p>
+    </div>
+    </div>`;
+  }
+  
+  console.log(teamCard)
+  teamContainer.innerHTML = teamCard;
+  
+
 
 
 
