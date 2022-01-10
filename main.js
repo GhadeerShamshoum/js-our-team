dbf04e335d18e96da1f7cad9940ac726ba698b40
Ghadeer <ghaora@gmail.com>
2022-01-10 15:40:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+itemsContainer = '';
+let teamCard = document.querySelector('.team-card');
+console.log(team.name)
+for(let i=0; i<team.length; i++){
+  itemsContainer +=` <div class="team-card">
+    <div class="card-image">
+      <img
+        src='"${team[i].image}"'
+        alt='"${team[i].image}"'
+      />
+    </div>
+    <div class="card-text">
+      <h3>${team[i].name}</h3>
+      <p>${team[i].role}</p>
+    </div>
+  </div>`;
+}
+
+
+teamCard.innerHTML = itemsContainer;
+
+
+
